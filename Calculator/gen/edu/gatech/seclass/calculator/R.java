728b1de82ea1f5fb237b5cba4befec9ad070c01b
Hui Gao <hgao64@gatech.edu>
2015-02-15 23:54:44 -0500

    Updated main activity and layout

--- Calculator/gen/edu/gatech/seclass/calculator/R.java
+++ Calculator/gen/edu/gatech/seclass/calculator/R.java
@@ -10,15 +10,52 @@ package edu.gatech.seclass.calculator;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f050000;
+        public static final int green=0x7f050001;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+    }
+    public static final class id {
+        public static final int button1=0x7f070002;
+        public static final int button10=0x7f070009;
+        public static final int button11=0x7f07000a;
+        public static final int button12=0x7f07000b;
+        public static final int button13=0x7f07000c;
+        public static final int button14=0x7f07000d;
+        public static final int button15=0x7f07000e;
+        public static final int button2=0x7f070001;
+        public static final int button3=0x7f070000;
+        public static final int button4=0x7f070003;
+        public static final int button5=0x7f070005;
+        public static final int button6=0x7f070004;
+        public static final int button7=0x7f070006;
+        public static final int button8=0x7f070007;
+        public static final int button9=0x7f070008;
+        public static final int textView1=0x7f070010;
+        public static final int textView2=0x7f07000f;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040002;
+        public static final int button1=0x7f040002;
+        public static final int button10=0x7f04000b;
+        public static final int button11=0x7f04000c;
+        public static final int button12=0x7f04000d;
+        public static final int button13=0x7f04000e;
+        public static final int button14=0x7f04000f;
+        public static final int button15=0x7f040010;
+        public static final int button2=0x7f040003;
+        public static final int button3=0x7f040004;
+        public static final int button4=0x7f040005;
+        public static final int button5=0x7f040006;
+        public static final int button6=0x7f040007;
+        public static final int button7=0x7f040008;
+        public static final int button8=0x7f040009;
+        public static final int button9=0x7f04000a;
         public static final int title_activity_main=0x7f040001;
     }
     public static final class style {
@@ -43,10 +80,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- Calculator/src/edu/gatech/seclass/calculator/MainActivity.java
+++ Calculator/src/edu/gatech/seclass/calculator/MainActivity.java
@@ -1,15 +1,206 @@
 package edu.gatech.seclass.calculator;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements android.view.View.OnClickListener {
+	
+	TextView txt;
+	Button btn1;
+	Button btn2;
+	Button btn3;
+	Button btn4;
+	Button btn5;
+	Button btn6;
+	Button btn7;
+	Button btn8;
+	Button btn9;
+	Button btn10;
+	Button btn12;
+	Button btn13;
+	Button btn11;
+	Button btn14;
+	Button btn15;
+	long operand1 = 0;
+	long operand2 = 0;
+	long result = 0;
+	String operator = "";
+	String input = "";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button btn1 = (Button) findViewById(R.id.button1);
+		btn1.setOnClickListener(this);
+		Button btn2 = (Button) findViewById(R.id.button2);
+		btn2.setOnClickListener(this);
+		Button btn3 = (Button) findViewById(R.id.button3);
+		btn3.setOnClickListener(this);
+		Button btn4 = (Button) findViewById(R.id.button4);
+		btn4.setOnClickListener(this);
+		Button btn5 = (Button) findViewById(R.id.button5);
+		btn5.setOnClickListener(this);
+		Button btn6 = (Button) findViewById(R.id.button6);
+		btn6.setOnClickListener(this);
+		Button btn7 = (Button) findViewById(R.id.button7);
+		btn7.setOnClickListener(this);
+		Button btn8 = (Button) findViewById(R.id.button8);
+		btn8.setOnClickListener(this);
+		Button btn9 = (Button) findViewById(R.id.button9);
+		btn9.setOnClickListener(this);
+		Button btn0 = (Button) findViewById(R.id.button10);
+		btn0.setOnClickListener(this);
+		Button btnC = (Button) findViewById(R.id.button11);
+		btnC.setOnClickListener(this);
+		Button btnSum = (Button) findViewById(R.id.button12);
+		btnSum.setOnClickListener(this);
+		Button btnMinus = (Button) findViewById(R.id.button13);
+		btnMinus.setOnClickListener(this);
+		Button btnMulti = (Button) findViewById(R.id.button14);
+		btnMulti.setOnClickListener(this);
+		Button btnEqual = (Button) findViewById(R.id.button15);
+		btnEqual.setOnClickListener(this);
+		txt = (TextView) findViewById(R.id.textView1);
+		
+	}
+
+	public void onClick(View view) {
+		
+		switch (view.getId()) {
+		case R.id.button1:
+			if ((Long.parseLong(txt.getText().toString()) == 0) || (input == "")) {
+				txt.setText("");
+			}
+			txt.setText(txt.getText().toString() + ((Button) view).getText().toString());
+			input = txt.getText().toString();
+			txt.setText(input);
+			break;
+		case R.id.button2:
+			if ((Long.parseLong(txt.getText().toString()) == 0) || (input == "")) {
+				txt.setText("");
+			}
+			txt.setText(txt.getText().toString() + ((Button) view).getText().toString());
+			input = txt.getText().toString();
+			txt.setText(input);
+			break;
+		case R.id.button3:
+			if ((Long.parseLong(txt.getText().toString()) == 0) || (input == "")) {
+				txt.setText("");
+			}
+			txt.setText(txt.getText().toString() + ((Button) view).getText().toString());
+			input = txt.getText().toString();
+			txt.setText(input);
+			break;
+		case R.id.button4:
+			if ((Long.parseLong(txt.getText().toString()) == 0) || (input == "")) {
+				txt.setText("");
+			}
+			txt.setText(txt.getText().toString() + ((Button) view).getText().toString());
+			input = txt.getText().toString();
+			txt.setText(input);
+			break;
+		case R.id.button5:
+			if ((Long.parseLong(txt.getText().toString()) == 0) || (input == "")) {
+				txt.setText("");
+			}
+			txt.setText(txt.getText().toString() + ((Button) view).getText().toString());
+			input = txt.getText().toString();
+			txt.setText(input);
+			break;
+		case R.id.button6:
+			if ((Long.parseLong(txt.getText().toString()) == 0) || (input == "")) {
+				txt.setText("");
+			}
+			txt.setText(txt.getText().toString() + ((Button) view).getText().toString());
+			input = txt.getText().toString();
+			txt.setText(input);
+			break;
+		case R.id.button7:
+			if ((Long.parseLong(txt.getText().toString()) == 0) || (input == "")) {
+				txt.setText("");
+			}
+			txt.setText(txt.getText().toString() + ((Button) view).getText().toString());
+			input = txt.getText().toString();
+			txt.setText(input);
+			break;
+		case R.id.button8:
+			if ((Long.parseLong(txt.getText().toString()) == 0) || (input == "")) {
+				txt.setText("");
+			}
+			txt.setText(txt.getText().toString() + ((Button) view).getText().toString());
+			input = txt.getText().toString();
+			txt.setText(input);
+			break;
+		case R.id.button9:
+			if ((Long.parseLong(txt.getText().toString()) == 0) || (input == "")) {
+				txt.setText("");
+			}
+			txt.setText(txt.getText().toString() + ((Button) view).getText().toString());
+			input = txt.getText().toString();
+			txt.setText(input);
+			break;
+		case R.id.button10:
+			if ((Long.parseLong(txt.getText().toString()) == 0) || (input == "")) {
+				txt.setText("");
+			}
+			txt.setText(txt.getText().toString() + ((Button) view).getText().toString());
+			input = txt.getText().toString();
+			txt.setText(input);
+			break;
+		case R.id.button11:
+			operand1 = 0;
+			operand2 = 0;
+			operator = "";
+			txt.setText(Long.toString(operand1));
+			break;
+		case R.id.button12:
+			operand1 = Long.parseLong(txt.getText().toString());
+			input = "";
+			operator = "+";
+			operand2 = 0;
+			break;
+		case R.id.button13:
+			operand1 = Long.parseLong(txt.getText().toString());
+			input = "";
+			operator = "-";
+			operand2 = 0;
+			break;
+		case R.id.button14:
+			operand1 = Long.parseLong(txt.getText().toString());
+			input = "";
+			operator = "*";
+			operand2 = 0;
+			break;
+		case R.id.button15:
+			operand2 = Long.parseLong(txt.getText().toString());
+			if (operator == "+") {
+				result = operand1 + operand2;
+				txt.setText(Long.toString(result));
+			}
+			else if (operator == "-") {
+				result = operand1 - operand2;
+				txt.setText(Long.toString(result));
+			}
+			else if (operator == "*") {
+				result = operand1 * operand2;
+				txt.setText(Long.toString(result));
+			}
+			else if (operator == "") {
+				txt.setText(txt.getText());
+			}
+			operand1 = 0;
+			operand2 = 0;
+			operator = "";
+			input = "";
+			break;
+		}		
 	}
 }
